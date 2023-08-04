148a6e969e71a17fcf1a300528e351a877c6edbf
Charles Huffman <huffmancharles14@gmail.com>
2023-08-04 12:40:37 -0500

    Added our sign in component as well as firbase utils and firebase libraries

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation-component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
-import Directory from "./components/directory/directory-component";
-
-const App = () => {
-  // eslint-disable-next-line
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-  /* this app.js is what holds all of our components in jsx form?
-    our next challenge is to create a category-directory. it will house each category hats, sneakers etc
-    In the categories directory we will move our array and our categories.map method.
-    in the app.js we will return <Categories /> like so and still have the same looking application
-*/
-  return <Directory categories={categories} />;
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
 };
 
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
+};
 export default App;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUxEflvI6nwMACC8elTx5_nwc7yKRCRrY",
+  authDomain: "crwn-db-ba96c.firebaseapp.com",
+  projectId: "crwn-db-ba96c",
+  storageBucket: "crwn-db-ba96c.appspot.com",
+  messagingSenderId: "382850290792",
+  appId: "1:382850290792:web:7e8f240ddc3227aec697dd",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapShot = await getDoc(userDocRef);
+
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+};
