c7499cdfd621face474fce8d8236ad0aee6afeb6
Charles Huffman <huffmancharles14@gmail.com>
2023-09-03 15:48:58 -0500

    Made some last minute touches before deploying

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -3,38 +3,11 @@ import { Outlet } from "react-router-dom";
 import Directory from "../../components/directory/directory-component";
 
 const Home = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
   // below in our return. Our outlet is our child component in the App.js file. If we put the Outlet below the Directory component the child component will render below the directory component. IE if we were to use a NavBar component we would want the Outlet to be on top. If we had a footer component we would put it below the directory component to keep these components Persistent
   return (
     <div>
       <Outlet />
-      <Directory categories={categories} />
+      <Directory />
     </div>
   );
 };
